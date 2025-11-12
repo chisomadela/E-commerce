aa57e1dbe87b3d228602754e0e4b8646af515f86
chisomadela <chisomenyika5@gmail.com>
2025-11-12 08:45:35 +0100

    more progress made

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
+import { Carousel } from "@/components/carousel";
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -9,16 +12,38 @@ export default async function Home() {
   });
 
   console.log(products);
-  return <div>
-    <section>
-      
-      <div>
-        
-        <div>
-          
+  return( 
+  <div>
+    <section className="rounded bg-neutral-100 py-8 sm:py-12">
+       <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16">
+        <div className="max-w-md space-y-4">
+          <h2 className="text-3xl font-bold tracking-tight md:text-4xl">Welcome to My Ecommerce</h2>
+          <p className="text-neutral-600">
+            Discover the latest products at the best prices.
+            </p>
+          <Button
+           asChild 
+          variant="default"
+          className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black te">
+            <Link 
+            href="/products"
+            className="inline-flex items-center justify-center rounded-full px-6 py-3">
+            Browse All Products
+            </Link>
+          </Button>
         </div>
+      <Image 
+      src= "/homeimage.jpg"
+       alt="Banner Image"
+       className="rounded"
+        width={450}
+         height={450}/>
       </div>
     </section>
-    </div>;
+    <section className="py-8"> 
+      <Carousel/> 
+      </section>
+    </div>
+    );
 }
 
