6e9e115a26b7622036b678a010bdaf88d542fca5
chisomadela <chisomenyika5@gmail.com>
2025-11-13 15:06:57 +0100

    finished handling the product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
 import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
+import Link from "next/link"
 import { Carousel } from "@/components/carousel";
 
 export default async function Home() {
@@ -15,7 +14,7 @@ export default async function Home() {
   return( 
   <div>
     <section className="rounded bg-neutral-100 py-8 sm:py-12">
-       <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16">
+       <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
         <div className="max-w-md space-y-4">
           <h2 className="text-3xl font-bold tracking-tight md:text-4xl">Welcome to My Ecommerce</h2>
           <p className="text-neutral-600">
@@ -24,7 +23,7 @@ export default async function Home() {
           <Button
            asChild 
           variant="default"
-          className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black te">
+          className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white">
             <Link 
             href="/products"
             className="inline-flex items-center justify-center rounded-full px-6 py-3">
@@ -41,7 +40,7 @@ export default async function Home() {
       </div>
     </section>
     <section className="py-8"> 
-      <Carousel/> 
+      <Carousel products= {products.data}/> 
       </section>
     </div>
     );
